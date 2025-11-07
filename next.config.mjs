4807c21b15152592c784f63928f460346a6c9042
kuhnbrandyn <kuhn.brandyn@gmail.com>
2025-11-06 19:27:44 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true },
+  webpack: (config) => {
+    config.externals.push("ffmpeg-static");
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["ffmpeg-static"],
+  },
 };
+
 export default nextConfig;
